ead56074f54daf3c1e82fbbf7012c9d240aee963
Tyler Becker <tyler.becker@selu.edu>
2016-03-03 00:52:02 -0600

    We made it

---
+++ WebAPI2/WebAPI2/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI2.Models;
+
+namespace WebAPI2.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public User GetUser()
+        {
+            User onlyUser = new User
+            {
+                Id = 1,
+                FirstName = "Seth",
+                LastName = "Champagne",
+                UserName = "champagneSeth",
+                Password = "Password"
+            };
+            return onlyUser;
+        }
+    }
+}
